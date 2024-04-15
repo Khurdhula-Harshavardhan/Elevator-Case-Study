ec64e2bca189e1e46c9a6e13bda16577fcd2dd1c
Harsha <khurdhulaharshavardhan@gmail.com>
2024-04-15 19:52:55 -0400

    working prototype for the entire system.

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from ElevatorControlSystem import ElevatorControlSystem
 from Floor import Floor
 from State import State
+import Elevator
 def main():
     # Initialize the elevator system with 2 elevators
-    ecs = ElevatorControlSystem(2)
+    total_floors = 5
+    ecs = ElevatorControlSystem(total_floors)
+
+    while True:
+        floor = int(input("Please enter the destination floor: "))
+        
+     
+        if floor>total_floors:
+            print("Sorry, invalid floor number, please enter a valid floor number: ")
+            continue
+
+        current_floor = Floor(floor)
+        
+        print("*"*50)
+        # Simulate floor requests
+        print("Floor %d calls an elevator."%(floor))
+        current_floor.call_elevator()
+        ecs.request_elevator(current_floor.floor_number)
+        print("*"*50)
+        
+
     
-    # Create floors
-    floor1 = Floor(1)
-    floor2 = Floor(2)
-
-    # Simulate floor requests
-    print("Floor 1 calls an elevator.")
-    floor1.call_elevator()
-    ecs.request_elevator(floor1.floor_number)
-
-    print("\nFloor 2 calls an elevator.")
-    floor2.call_elevator()
-    ecs.request_elevator(floor2.floor_number)
-
 if __name__ == "__main__":
     main()
